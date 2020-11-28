9dd0711d717750e313906356bfdb208fe27226d7
brecert <11599528+Brecert@users.noreply.github.com>
2020-11-27 20:32:44 -0500

    python: use `isleap` in calendar - added __pycache__ to gitignore - added test script to makefile

--- python/bree/__init__.py
+++ python/bree/__init__.py
@@ -1,5 +1,6 @@
 # bree
 # determines whether I am bree on any given date
+from calendar import isleap
 
 __all__ = ("is_bree",)
 
@@ -8,7 +9,5 @@ def is_bree(d):
 	return d.weekday() == 4 and\
 	  d.day - 7 > 0 and\
 	  d.day - 14 <= 0 and\
-	  (d.month != 3 or\
-	    not (d.year % 400 == 0 or\
-		  (d.year % 100 != 0 and d.year % 4 == 0))) and\
+	  (d.month != 3 or not isleap(d.year)) and\
 	  (d.month != 7 or d.year != 2023)
